8701eaead334e4f99b05867072a348a6f6eee223
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-02-21 11:58:55 +0900

    Update boxdrawing.js

--- boxdrawing.js
+++ boxdrawing.js
@@ -29,6 +29,40 @@ fn.headmark=(s,n,mark,emp)=>{
  return s.split('\n').map((d,i)=>(i===n)?mark+d:emp+d).join('\n')
 }
 
+const s2bmap=(()=>{
+ var aftnum = new Array("０","１","２","３","４","５","６","７","８","９");
+ var aftalph = new Array("Ａ","Ｂ","Ｃ","Ｄ","Ｅ","Ｆ","Ｇ","Ｈ","Ｉ","Ｊ",
+                         "Ｋ","Ｌ","Ｍ","Ｎ","Ｏ","Ｐ","Ｑ","Ｒ","Ｓ","Ｔ",
+                         "Ｕ","Ｖ","Ｗ","Ｘ","Ｙ","Ｚ",
+                         "ａ","ｂ","ｃ","ｄ","ｅ","ｆ","ｇ","ｈ","ｉ","ｊ",
+                         "ｋ","ｌ","ｍ","ｎ","ｏ","ｐ","ｑ","ｒ","ｓ","ｔ",
+                         "ｕ","ｖ","ｗ","ｘ","ｙ","ｚ");
+ var aftkigo = new Array('　','！','”','＃','＄','％','＆','’','（','）','＝','～','＾','｜',
+                         '‘','＠','｛','［','「','＋','；','＊','：','｝','］','」','＜','，','、','＞','．','。','？','・','／','＿');
+ var befnum = new Array("0","1","2","3","4","5","6","7","8","9");
+ var befalph = new Array("A","B","C","D","E","F","G","H","I","J",
+                         "K","L","M","N","O","P","Q","R","S","T",
+                         "U","V","W","X","Y","Z",
+                         "a","b","c","d","e","f","g","h","i","j",
+                         "k","l","m","n","o","p","q","r","s","t",
+                         "u","v","w","x","y","z");
+ var befkigo = new Array(' ','!','\"','#','\\$','%','&','\'','\\(','\\)','=','~','\\^','\\|',
+                         '`','@','\\{','\\[','｢','\\+',';','\\*',':','\\}','\\]','｣','<',',','､','>','\\.','｡','\\?','･','\\/','_');
+ var aft = aftnum.concat(aftalph , aftkigo); 
+ var bef = befnum.concat(befalph , befkigo);
+ return {bef:bef,aft:aft}
+ // s=s.replace(/\\/g,"￥"); is special
+})();
+fn.s2b=(s)=>{
+ var str=s,reg,i=0
+ for(i=0; i<s2bmap.aft.length; i++){
+  reg = new RegExp(s2bmap.bef[i],"g");
+  s=s.replace(reg, s2bmap.aft[i]);
+ }
+ s=s.replace(/\\/g,"￥");//special
+ return s;
+}
+
 /*
 let name='シキシャ'
 ,stats='毒！２００｀２０＠２５'
@@ -46,7 +80,7 @@ let boxdrawing=(text,w,h,lcr,strtr,strbr,type2flg)=>{
  if(type2flg) bar = '─',vtx = '│',tl = '┌',tr = '┐',bl = '└',br = '┘'
  ;
  let spbar=fn.fstr(sp,w)
- let t=text+'\n'+Array.from({length:h}).map(d=>spbar).join('\n')
+ let t=fn.s2b(text)+'\n'+Array.from({length:h}).map(d=>spbar).join('\n')
  let bars=fn.fstr(bar,w)
  ,f=(lcr==='r')?fn.lpad:(lcr==='c')?fn.cpad:fn.rpad
  ,body=t.split('\n').slice(0,h).map((d,i,o)=>{
