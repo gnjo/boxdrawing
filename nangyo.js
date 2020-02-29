1150c989b771fbe795e6ade94fbc01abc49e38db
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-02-29 17:39:32 +0900

    Create nangyo.js

---
+++ nangyo.js
@@ -0,0 +1,11 @@
+/*
++----------------------+  < line 1
+|tl        tc        tr|  < line 2 head
+|    <0>  <1>  <2>     |  < line 3 body1
+|cl  <3> <4>cc <5>   cr|  < line 4 body center
+|    <6>  <7>  <8>     |  < line 5 body3
+|bl        bc        br|  < line 6 foot
++----------------------+  < line 7 
+*/
+
+//change2zeroeight
